ec411ea09c33f9723c83e710ff99ba31c68d8c3a
patod01 <patocv2@hotmail.com>
2022-10-09 13:28:53 -0300

    GUI - Say hello to Sanjiro! And look at the new cables
    - added css classes to show unavailable buttons.
    - made API response actions for 'buy' and 'sell' buttons.
    - say hello to Sanji, the Robin's dog!
    	- sets the current directory to 'action'.
    	- returns the wallet's content.
    - made API test calls.
    	- added Insomnia API test json and yaml files.

--- app/going_merry/robin.py
+++ app/going_merry/robin.py
@@ -6,34 +6,56 @@ from flask import (
 
 app = Flask(__name__, template_folder='treasures')
 
+
 @app.errorhandler(404)
 def go_default(error):
-     return 'notmyproblem', 404
+     return 'notmyproblem .!.', 404
+
 
 @app.route('/')
-def home():
-     return render_template('bird.html')
+def home(): return render_template('bird.html')
 
-@app.route('/wallet/', methods=['POST', 'GET', 'PUT', 'DELETE'])
-def api():
-     print(os.getcwd())
-     if request.method == 'POST':
-          ...
-     elif request.method == 'GET':
+
+def sanji(kick_in_the_face: str|None = None) -> dict|None:
+     ("Robin's dog.")
+     if kick_in_the_face == 'Robin: Find my wallet!':
           if os.path.isdir('action'):
                os.chdir('action')
                print('Focus changed to actions folder.')
                print(os.getcwd())
+     else:
           if os.path.isfile('../db/chauchero'):
                wallet = open('../db/chauchero', 'r')
-               respuesta = json.load(wallet)
+               pocket = json.load(wallet)
           else:
-               respuesta = 'no existe la chauchera!!'
+               pocket = {}
+          return pocket
+     return None
+
+
+@app.route('/wallet/', methods=['POST', 'GET'])#, 'PUT', 'DELETE'])
+def api():
+     sanji('Robin: Find my wallet!')
+     my_pocket: dict
+     if request.method == 'POST':
+          if request.is_json:
+               if request.json['action'] == 'buy':
+                    os.system('start buy')
+               elif request.json['action'] == 'sell':
+                    os.system('start sell')
+               # elif request.json['action'] == 'ship':
+               #      os.system('start ship')
+               # elif request.json['action'] == 'spy':
+               #      os.system('start spy')
+               my_pocket = sanji().get('coin', 'No existe la chauchera!!')
+     elif request.method == 'GET':
+          my_pocket = sanji()
      elif request.method == 'PUT':
           ...
      elif request.method == 'DELETE':
           ...
-     return jsonify(respuesta)
+     return jsonify(my_pocket)
+
 
 if __name__ == '__main__':
      app.run(debug=True, port=7777, host='0.0.0.0')
